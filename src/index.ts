589de5b4c316f48f542f58199fb6120fc07cffca
Stepanko9 <73553838+Stepanko9@users.noreply.github.com>
2025-11-14 21:33:25 +0200

    Create index.ts to initialize MEXC Trade Copier application

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Logger from './utils/logger';
+const logger = new Logger('app.log');
+logger.logInfo('MEXC Trade Copier started successfully!');
+logger.logInfo('Initializing application...');
+export default logger;
